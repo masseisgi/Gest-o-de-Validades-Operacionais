127b9396ae2978a3f1ebdc9b2386863fd1b9438e
masseisgi <thiago@masseisgi.com>
2026-04-26 21:48:45 -0300

    Delete public/sw.js

--- public/sw.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => clients.claim());
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('/api') || e.request.url.includes('.netlify')) return;
-});
